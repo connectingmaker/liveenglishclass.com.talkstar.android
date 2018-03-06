a8d9f367270bfbb11b68b6f4abb273a46c4cbc53
kwangheejung <khjung@c-maker.co.kr>
2018-03-06 17:08:09 +0900

    명칭변경

--- app/src/main/java/liveenglishclass/com/talkstar/LoginActivity.java
+++ app/src/main/java/liveenglishclass/com/talkstar/LoginActivity.java
@@ -3,11 +3,11 @@ package liveenglishclass.com.talkstar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MemberJoinActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_member_join);
+        setContentView(R.layout.activity_login);
     }
 }
