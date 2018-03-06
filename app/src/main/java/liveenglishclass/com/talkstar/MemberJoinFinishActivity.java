6a4cfb13e16918d8f88308cd5944ff772a3dcd6d
kwangheejung <khjung@c-maker.co.kr>
2018-03-06 18:48:05 +0900

    회원가입완료 UI

---
+++ app/src/main/java/liveenglishclass/com/talkstar/MemberJoinFinishActivity.java
@@ -0,0 +1,13 @@
+package liveenglishclass.com.talkstar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MemberJoinFinishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_join_finish);
+    }
+}
