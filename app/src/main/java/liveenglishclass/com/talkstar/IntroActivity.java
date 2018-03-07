d7e3aee654c525dae43dbb009d075fc4fcf164fc
kwangheejung <khjung@c-maker.co.kr>
2018-03-07 23:31:45 +0900

    1. Activity Manager 추가 2. ApiService 추가 3. MainActivity 샘플 네틍워크 연동

--- app/src/main/java/liveenglishclass/com/talkstar/IntroActivity.java
+++ app/src/main/java/liveenglishclass/com/talkstar/IntroActivity.java
@@ -1,5 +1,6 @@
 package liveenglishclass.com.talkstar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,8 +11,13 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
-public class IntroActivity extends AppCompatActivity {
+import liveenglishclass.com.talkstar.core.ActivityManager;
 
+public class IntroActivity extends AppCompatActivity {
+    private ActivityManager actManager = ActivityManager.getInstance();
+
+
+    private Intent intent;
     private ImageView im_intro_logo;
 
     @Override
@@ -19,11 +25,38 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
+        actManager.addActivity(this);
+
         im_intro_logo = (ImageView) findViewById(R.id.im_intro_logo);
 
         Animation animation = AnimationUtils.loadAnimation(this,R.anim.intro_animation);
         im_intro_logo.startAnimation(animation);
         //view.startAnimation(animation);
+    }
+
+    public void btnClickEvent(View v) {
+        switch(v.getId())
+        {
+            case R.id.intro_btn_login:
+                intent = new Intent(IntroActivity.this, LoginActivity.class);
+                break;
+
+            case R.id.intro_btn_memberjoin:
+                intent = new Intent(IntroActivity.this, MemberJoinActivity.class);
+                break;
+
+        }
+
+        startActivity(intent);
+        //overridePendingTransition(R.anim.anim_slide_up, R.anim.anim_slide_down);
+        //finish();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        actManager.removeActivity(this);
     }
 
 }
--- app/src/main/java/liveenglishclass/com/talkstar/MemberJoinFinishActivity.java
+++ app/src/main/java/liveenglishclass/com/talkstar/MemberJoinFinishActivity.java
@@ -1,13 +1,45 @@
 package liveenglishclass.com.talkstar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import liveenglishclass.com.talkstar.core.ActivityManager;
 
 public class MemberJoinFinishActivity extends AppCompatActivity {
+    private ActivityManager actManager = ActivityManager.getInstance();
+    private Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_join_finish);
+
+        actManager.addActivity(this);
+
+    }
+
+    public void btnEventClick(View v) {
+        switch(v.getId()) {
+            case R.id.memberjoinfinish_btn_start:
+
+
+                /**** 진행된 Activity 전체 삭제 *****/
+                actManager.finishAllActivity();
+
+
+                intent = new Intent(MemberJoinFinishActivity.this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        actManager.removeActivity(this);
     }
 }
