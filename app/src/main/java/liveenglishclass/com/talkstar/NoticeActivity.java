cf311ff3dcf6de3d685c3704e23914c8fe181aac
jccho <jccho@c-maker.co.kr>
2018-03-07 23:36:38 +0900

    설정화면과 공지사항, 의견보내기 등 세부화면 (그림자포함

---
+++ app/src/main/java/liveenglishclass/com/talkstar/NoticeActivity.java
@@ -0,0 +1,17 @@
+package liveenglishclass.com.talkstar;
+
+/**
+ * Created by jccho on 2018. 3. 6..
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
